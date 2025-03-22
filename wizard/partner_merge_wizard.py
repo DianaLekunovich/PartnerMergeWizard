47b67da9311933bb3cd1a5442fbbd351988465fc
diana <kto2518@gmail.com>
2025-03-22 18:08:52 +0100

    Added email choice functionality

--- wizard/partner_merge_wizard.py
+++ wizard/partner_merge_wizard.py
@@ -6,25 +6,8 @@ class PartnerMergeWizard(models.TransientModel):
     _name = 'partner.merge.wizard'
     _description = 'Merge Partners Wizard'
 
-    def _get_selection(self):
-        selection=[("huih", "yugyugyg")]
-        selection.append(("uj", self.main_partner_id.email))
-        return selection
-
     partner_ids = fields.Many2many('res.partner', string='Partners to Merge', required=True)
     main_partner_id = fields.Many2one('res.partner', string='Main Partner', required=True, domain="[('id', 'in', partner_ids)]")
-
-    email_choice = fields.Selection([
-        ('main', 'Use main partner email'),
-        ('other', 'Choose from duplicates'),
-    ], default='main')
-    email_options = fields.Selection(
-        selection='_compute_email_options_selection',
-        string='Choose email',
-        readonly=False,
-        #compute='_compute_email_options_selection',
-        #inverse='_inverse_default_email'
-    )
 
     @api.model
     def default_get(self, fields):
@@ -33,7 +16,6 @@ class PartnerMergeWizard(models.TransientModel):
         if active_ids:
             partners = self.env['res.partner'].browse(active_ids)
             res['partner_ids'] = [(6, 0, partners.ids)]
-        print("get")
         return res
 
     @api.constrains('partner_ids')
@@ -41,50 +23,6 @@ class PartnerMergeWizard(models.TransientModel):
         for record in self:
             if len(record.partner_ids) < 2:
                 raise UserError(_("You must select at least two partners to merge."))
-
-    def _get_email_options(self):
-        """Returns the list of available email addresses"""
-        options = []
-        if self.email_choice == 'other' and self.partner_ids:
-            print("get email func")
-            for partner in self.partner_ids:
-                if partner.email and partner.email not in [option[0] for option in options]:
-                    options.append((partner.email, partner.email))
-        return options
-
-    #@api.depends('partner_ids', 'main_partner_id')
-    @api.model
-    def _compute_email_options_selection(self):
-        """Dynamic selection options for email_options"""
-        options = [("dianalekunovic@gmail.com","bkhkjnkn")]
-        #print(self.partner_ids.ids)
-        print(self.partner_ids.ids)
-        if self.partner_ids.ids:
-            for id in self.partner_ids.ids:
-                email = self.env['res.partner'].browse(id).email
-                if email:
-                    options.append((email, email))
-
-        """if self.main_partner_id.email and self.main_partner_id.email not in [option[0] for option in options]:
-            options.insert(0, (self.main_partner_id.email, self.main_partner_id.email))"""
-        #print(options)
-        #self._fields['email_options'].selection = options
-        return options
-
-    @api.onchange('partner_ids', 'email_choice', 'main_partner_id')
-    def _onchange_email_options(self):
-        self._compute_email_options_selection()
-
-    @api.depends('email_choice', 'partner_ids', 'main_partner_id')
-    def _compute_email_options(self):
-        if self.email_choice == 'main':
-            self.email_options = self.main_partner_id.email if self.main_partner_id.email else False
-        else:
-            options = []
-            for partner in self.partner_ids:
-                if partner.email and partner.email not in [option[0] for option in options]:
-                    options.append((partner.email, partner.email))
-            self.email_options = options[0][0] if options else False
 
     def action_merge(self):
         self.ensure_one()
@@ -99,6 +37,73 @@ class PartnerMergeWizard(models.TransientModel):
             sale_orders = self.env['sale.order'].search([('partner_id', '=', partner_id)])
             sale_orders.write({'partner_id': self.main_partner_id.id})
 
-            self.env['res.partner'].browse(partner_id).write({'active': False})
+        unique_emails = list(set(self.partner_ids.mapped("email")))
+        if len(unique_emails) > 1:
+            return {
+                'name': _('Choose Email'),
+                'type': 'ir.actions.act_window',
+                'res_model': 'partner.merge.email.wizard',
+                'view_mode': 'form',
+                'target': 'new',
+                'context': {'default_partner_merge_wizard_id': self.id,
+                            'partner_ids_to_archive': partner_ids_to_merge}
+            }
+        else:
+            for partner_id in partner_ids_to_merge:
+                self.env['res.partner'].browse(partner_id).write({'active': False})
+            return {'type': 'ir.actions.act_window_close'}
 
+class PartnerMergeEmailWizard(models.TransientModel):
+    _name = 'partner.merge.email.wizard'
+    _description = 'Choose Email for Merged Partner'
+
+    partner_merge_wizard_id = fields.Many2one(
+        'partner.merge.wizard',
+        string="Merge Wizard",
+        required=True
+    )
+    email_line_ids = fields.One2many(
+        'partner.merge.email.line',
+        'wizard_id',
+        string="Emails"
+    )
+
+    @api.model
+    def default_get(self, fields):
+        res = super(PartnerMergeEmailWizard, self).default_get(fields)
+        merge_wizard = self.env['partner.merge.wizard'].browse(self._context.get('default_partner_merge_wizard_id'))
+
+        if merge_wizard.exists():
+            emails = list(set(merge_wizard.partner_ids.mapped("email")))
+            email_lines = [(0, 0, {'email': email}) for email in emails if email]
+            res['email_line_ids'] = email_lines
+        return res
+
+    @api.constrains('email_line_ids')
+    def _check_only_one_email_selected(self):
+        for wizard in self:
+            selected_emails = wizard.email_line_ids.filtered(lambda line: line.selected)
+            if len(selected_emails) != 1:
+                raise UserError(_("You must select exactly one email to continue."))
+
+    def action_confirm_email(self):
+        selected_email = self.email_line_ids.filtered(lambda e: e.selected)
+        if self.partner_merge_wizard_id and selected_email:
+            self.partner_merge_wizard_id.main_partner_id.write({'email': selected_email.email})
+        for partner_id in self._context.get('partner_ids_to_archive'):
+            self.env['res.partner'].browse(partner_id).write({'active': False})
         return {'type': 'ir.actions.act_window_close'}
+
+class PartnerMergeEmailLine(models.TransientModel):
+    _name = 'partner.merge.email.line'
+    _description = 'Email Selection Line'
+
+    wizard_id = fields.Many2one(
+        'partner.merge.email.wizard',
+        string="Wizard",
+        required=True,
+        ondelete='cascade'
+    )
+    email = fields.Char(string="Email", required=True)
+    selected = fields.Boolean(string="Use this email")
+
