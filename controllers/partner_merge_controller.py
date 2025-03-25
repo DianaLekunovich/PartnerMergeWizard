20142fc4c1cfc26bd93488e0ed132ff4af89149c
diana <kto2518@gmail.com>
2025-03-26 00:19:12 +0100

    Add partner merge controller

---
+++ controllers/partner_merge_controller.py
@@ -0,0 +1,20 @@
+from odoo import http
+from odoo.http import request
+import json
+
+class PartnerMergeController(http.Controller):
+
+    @http.route('/partner/merge', type='json', auth='user')
+    def merge_partners(self, partner_ids, main_partner_id):
+
+        try:
+            partner_ids = [int(partner_id) for partner_id in partner_ids]
+            main_partner_id = int(main_partner_id)
+            wizard = request.env['partner.merge.wizard'].create({
+                'partner_ids': [(6, 0, partner_ids)],
+                'main_partner_id': main_partner_id
+            })
+            wizard.action_merge()
+            return {'success': True, 'message': 'Partners merged successfully.'}
+        except Exception as e:
+            return {'success': False, 'message': str(e)}
